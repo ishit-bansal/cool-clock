c72ac6634baaf42c07c813c55e27915663eaf5e8
ishitbansal <72ishit@gmail.com>
2026-01-02 20:18:40 -0600

    add js to control hand rotation angles

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get the clock hands
+const hands = document.querySelectorAll('.hand');
+
+// Set different angles for each hand
+hands[0].style.transform = 'rotate(45deg)';
+hands[1].style.transform = 'rotate(135deg)';
