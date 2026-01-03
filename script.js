69a12f8fa8597f43f9965c149175595099d413d6
ishitbansal <72ishit@gmail.com>
2026-01-02 20:19:22 -0600

    add angle mapping for different clock positions

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-// Create a grid of 24 clocks (6 rows x 4 columns)
+// Mapping of positions to angles
+// Each clock can form different shapes based on hand angles
+// Angles: 0 = right, 90 = down, 180 = left, -90 = up
+
+const angles = {
+  'corner-tl': [90, 0],    // top-left corner: down + right
+  'corner-tr': [90, 180],  // top-right corner: down + left
+  'corner-bl': [-90, 0],   // bottom-left: up + right
+  'corner-br': [-90, 180], // bottom-right: up + left
+  'horizontal': [0, 180],  // horizontal line
+  'vertical': [-90, 90],   // vertical line
+  'blank': [135, 135]      // neutral position
+};
+
+// Create the grid
 const grid = document.getElementById('grid');
 
 for (let i = 0; i < 24; i++) {
   const clock = document.createElement('div');
   clock.className = 'clock';
-  
-  // Add two hands to each clock
   clock.innerHTML = '<div class="hand"></div><div class="hand"></div>';
-  
   grid.appendChild(clock);
 }
+
+// Test: set first clock to corner-tl
+const firstClock = document.querySelector('.clock');
+const hands = firstClock.querySelectorAll('.hand');
+hands[0].style.transform = `rotate(${angles['corner-tl'][0]}deg)`;
+hands[1].style.transform = `rotate(${angles['corner-tl'][1]}deg)`;
